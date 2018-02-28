81fe6d9dadd4e25c943edaee18f29858d0e33281
arthuravilla7 <arthuravilla7@gmail.com>
2018-02-28 01:11:50 -0300

    altaracoes na tela de gestao de alunos

--- public/js/controller/GestaoAlunosCtrl.js
+++ public/js/controller/GestaoAlunosCtrl.js
@@ -3,16 +3,28 @@ angular.module('app').controller('gestaoAlunosCtrl',['$scope', '$state', 'apiSer
 function gestaoAlunosCtrl($scope, $state, apiService, apiConstantes){
     
     $scope.alunos = [];
+    $scope.qtdPendentes = 0;
+    $scope.qtdAtivos = 0;
+    $scope.qtdInativos = 0;
 
     function getAlunos(){
         apiService.get(apiConstantes.baseUrlAPI + apiConstantes.aluno).then(function(result){
-            console.log(result)
             $scope.alunos = result.data;
+            calculaDadosDashboard($scope.alunos)
         }, function(error){
             console.log(error)
         })
     }
     getAlunos();
+
+    function calculaDadosDashboard(arrayAlunos){
+        //to do
+    }
+
+    $scope.abrirModalFichaAluno = function(aluno){
+        $scope.alunoSelecionado = aluno;
+        $("#modal-ficha-aluno").modal("show");
+    }
 
     $scope.irNovoAluno = function(){
         $state.go('novoaluno')
